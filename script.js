74d71287effc6d964c237a007be50fc8910a2d33
Minhaz Halim <mhzim93@gmail.com>
2021-03-14 22:28:38 +0600

    remove uppercase

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('keyup',filterNames);
 function filterNames(){
-     let filterValue = document.getElementById('filterInput').value.toUpperCase();
+     let filterValue = document.getElementById('filterInput').value;
      let unorderedList = document.getElementById('names');
      let list = unorderedList.querySelectorAll('.collection-item');
      for(let i=0;i<list.length;i++){
           let a = list[i].getElementsByTagName('a')[0];
-          if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
+          if(a.innerHTML.indexOf(filterValue)>-1){
                list[i].style.display = '';
           }else{
                list[i].style.display = 'none';
