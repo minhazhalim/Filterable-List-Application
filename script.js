c6d7ad901a7d9fb14184d0fbb2b63cd2501ab54d
Minhaz Halim <mhzim93@gmail.com>
2021-03-14 17:13:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const filterInput = document.getElementById('filterInput');
+filterInput.addEventListener('keyup',filterNames);
+function filterNames(){
+     let filterValue = document.getElementById('filterInput').value.toUpperCase();
+     let unorderedList = document.getElementById('names');
+     let list = unorderedList.querySelectorAll('.collection-item');
+     for(let i=0;i<list.length;i++){
+          let a = list[i].getElementsByTagName('a')[0];
+          if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
+               list[i].style.display = '';
+          }else{
+               list[i].style.display = 'none';
+          }
+     }
+}
